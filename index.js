920b452c96b62fdade526b567a71bd9d6b76bd12
SeanConcepcion29 <siconcepcion@up.edu.ph>
2023-02-27 08:46:34 +0800

    some changes

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+const math = require('./math.js');
+
+console.log(math.add(5,7));
+console.log(math.mul(5,7));
+console.log(math.PI);
+console.log(math.obj.one + math.obj.two);
+*/
+
+//import math from './math.js'
+import {add as sum, PI} from './math.js'
+
+console.log(math.sum(5,7));
+
+//console.log(sum(5,7));
